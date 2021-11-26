d22b0dd4be53c8159948b99fbe92c969a3e0e4c5
gecmanow <gecmanow@gmail.com>
2021-11-27 04:57:23 +0700

    transferring project to laravel

--- resources/views/bobcat.blade.php
+++ resources/views/bobcat.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.home')
 
-@section('title', 'Заказать минипогрузчик в Красноярске')
-@section('description', 'Минипогрузчики в Красноярске за 40 мин. от 1000 руб. Круглосуточно. Оперативно.')
-@section('keywords', '')
+@section('title', 'Заказать минипогрузчик (Bobcat) в Красноярске')
+@section('description', 'Минипогрузчики в Красноярске за 40 мин. от 1500 руб. Круглосуточно. Оперативно.')
+@section('keywords', 'минипогрузчик, bobcat, аренда минипогрузчика, заказать минипогрузчик красноярск')
 @section('ogimage'){{ asset('img/og/bobcat.jpg') }}@endsection
 
 @section('content')
@@ -63,7 +63,7 @@
                         <svg class="offer_item-icon ruble" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512">
                             <path d="M239.36 320C324.48 320 384 260.542 384 175.071S324.48 32 239.36 32H76c-6.627 0-12 5.373-12 12v206.632H12c-6.627 0-12 5.373-12 12V308c0 6.627 5.373 12 12 12h52v32H12c-6.627 0-12 5.373-12 12v40c0 6.627 5.373 12 12 12h52v52c0 6.627 5.373 12 12 12h58.56c6.627 0 12-5.373 12-12v-52H308c6.627 0 12-5.373 12-12v-40c0-6.627-5.373-12-12-12H146.56v-32h92.8zm-92.8-219.252h78.72c46.72 0 74.88 29.11 74.88 74.323 0 45.832-28.16 75.561-76.16 75.561h-77.44V100.748z"/>
                         </svg>
-                        <span class="offer_item-text">от 1000 руб.</span>
+                        <span class="offer_item-text">от 1500 руб.</span>
                     </div>
                     <div class="offer_item offer_item-no-mitm">
                         <span class="offer_item-text">Без посредников и переплат</span>
